93ecf0ecee45a093d1b2d3ad0a2685e4d659745d
Bastian Isensee <bisensee@bastian-isensee.com>
2024-03-30 21:04:13 +0100

    Get single todo endpoint

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 var allTodos = []Todo{
@@ -22,7 +23,7 @@ func handleWelcome(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Welcome to this Todo list application!")
 }
 
-func handleTodoPost(writer http.ResponseWriter, request *http.Request) {
+func handlePost(writer http.ResponseWriter, request *http.Request) {
 	body, err := ioutil.ReadAll(request.Body)
 	if err != nil {
 		http.Error(writer, "Failed to read request body", http.StatusBadRequest)
@@ -42,7 +43,7 @@ func handleTodoPost(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusCreated)
 }
 
-func handleTodoGetAll(writer http.ResponseWriter, request *http.Request) {
+func handleGetAll(writer http.ResponseWriter, request *http.Request) {
 	data, err := json.Marshal(allTodos)
 
 	if err != nil {
@@ -54,14 +55,46 @@ func handleTodoGetAll(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "%s", data)
 }
 
+func handleGet(writer http.ResponseWriter, request *http.Request) {
+	rawIndex := request.PathValue("index")
+	if rawIndex == "" {
+		http.Error(writer, "Missing index value in path", http.StatusBadRequest)
+		return
+	}
+
+	index, err := strconv.Atoi(rawIndex)
+	if err != nil {
+		http.Error(writer, "Index value in path must be an integer", http.StatusBadRequest)
+		return
+	}
+
+	if index >= len(allTodos) {
+		http.Error(writer, "Index value in path must be smaller than todo list length", http.StatusBadRequest)
+		return
+	}
+
+	todo := allTodos[index]
+	data, err := json.Marshal(todo)
+
+	if err != nil {
+		http.Error(writer, "Error on response writing", http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(writer, "%s", data)
+
+}
+
 func makeMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	// Important: Make sure your Go version is 1.22.1 or higher as
 	// there have been multiple changes to the mux routing.
 	mux.HandleFunc("GET /{$}", handleWelcome) // Note {$} to match exactly root.
-	mux.HandleFunc("GET /todo/all", handleTodoGetAll)
-	mux.HandleFunc("POST /todo", handleTodoPost)
+	mux.HandleFunc("GET /todo/{index}", handleGet)
+	mux.HandleFunc("GET /todo/all", handleGetAll)
+	mux.HandleFunc("POST /todo", handlePost)
 
 	return mux
 }
--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -10,7 +11,7 @@ import (
 	"testing"
 )
 
-func TestCrudApi(t *testing.T) {
+func TestTodoApi(t *testing.T) {
 	server := httptest.NewServer(makeMux())
 	defer server.Close()
 	t.Logf("Server started at %s", server.URL)
@@ -26,18 +27,28 @@ func TestCrudApi(t *testing.T) {
 	}
 
 	t.Run("Get all initial todos", func(t *testing.T) {
-		todoGetAll(t, expected, server.URL)
+		getAll(t, expected, server.URL)
 	})
+
+	t.Run("Get a specific todo", func(t *testing.T) {
+		lastIndex := len(expected) - 1
+		get(t, lastIndex, expected[lastIndex], server.URL)
+	})
+
 	t.Run("Post a new todo", func(t *testing.T) {
 		todo := Todo{Name: "eat some vegetables"}
-		todoPost(t, todo, server.URL)
+		post(t, todo, server.URL)
 
 		expected = append(expected, todo)
-		todoGetAll(t, expected, server.URL)
+		getAll(t, expected, server.URL)
+
+		lastIndex := len(expected) - 1
+		get(t, lastIndex, expected[lastIndex], server.URL)
 	})
+
 }
 
-func todoPost(t *testing.T, todo Todo, baseUrl string) {
+func post(t *testing.T, todo Todo, baseUrl string) {
 	data, err := json.Marshal(todo)
 
 	if err != nil {
@@ -56,7 +67,35 @@ func todoPost(t *testing.T, todo Todo, baseUrl string) {
 	}
 }
 
-func todoGetAll(t *testing.T, expected []Todo, baseUrl string) {
+func get(t *testing.T, index int, expected Todo, baseUrl string) {
+	res, err := http.Get(fmt.Sprintf("%s/todo/%d", baseUrl, index))
+	if err != nil {
+		t.Fatalf("Error on http request: %v", err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status 200 but got %v", res.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Unexpected error on body reading: %v", err)
+	}
+
+	var actual Todo
+	err = json.Unmarshal(data, &actual)
+	if err != nil {
+		t.Fatalf("Unexpected error on body parsing: %v", err)
+	}
+
+	if expected != actual {
+		t.Fatalf("Expected todo %+v but got %+v", expected, actual)
+	}
+}
+
+func getAll(t *testing.T, expected []Todo, baseUrl string) {
 	res, err := http.Get(baseUrl + "/todo/all")
 	if err != nil {
 		t.Fatalf("Error on http request: %v", err)
